4f02ef55fbeae44b36d0bf7d4c9f499bb7e46f3f
dua1999 <94859612+dua1999@users.noreply.github.com>
2022-11-21 09:24:50 +0400

    Add files via upload

---
+++ HS/main.js
@@ -0,0 +1,103 @@
+const container = document.querySelector(".container");
+const cards = document.querySelector(".cards");
+
+/* keep track of user's mouse down and up */
+let isPressedDown = false;
+/* x horizontal space of cursor from inner container */
+let cursorXSpace;
+
+container.addEventListener("mousedown", (e) => {
+  isPressedDown = true;
+  cursorXSpace = e.offsetX - cards.offsetLeft;
+  container.style.cursor = "grabbing";
+});
+
+container.addEventListener("mouseup", () => {
+  container.style.cursor = "grab";
+});
+
+window.addEventListener("mouseup", () => {
+  isPressedDown = false;
+});
+
+container.addEventListener("mousemove", (e) => {
+  if (!isPressedDown) return;
+  e.preventDefault();
+  cards.style.left = `${e.offsetX - cursorXSpace}px`;
+  boundCards();
+});
+
+function boundCards() {
+  const container_rect = container.getBoundingClientRect();
+  const cards_rect = cards.getBoundingClientRect();
+
+  if (parseInt(cards.style.left) > 0) {
+    cards.style.left = 0;
+  } else if (cards_rect.right < container_rect.right) {
+    cards.style.left = `-${cards_rect.width - container_rect.width}px`;
+  }
+}
+
+container.addEventListener("mousemove", (e) => {
+    if (!isPressedDown) return;
+    e.preventDefault();
+    cards.style.right = `${e.offsetX - cursorXSpace}px`;
+    boundCards();
+  });
+  
+  function boundCards() {
+    const container_rect = container.getBoundingClientRect();
+    const cards_rect = cards.getBoundingClientRect();
+  
+    if (parseInt(cards.style.left) > 0) {
+      cards.style.left = 0;
+    } else if (cards_rect.right < container_rect.right) {
+      cards.style.right = `-${cards_rect.width - container_rect.width}px`;
+    }
+  }
+
+  function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  var countDownDate = new Date("Jan 5, 2023 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d : " + hours + "h : "
+  + minutes + "m : " + seconds + " s ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
